8e588d535da99e5ffe58feae821066e849e2b8b6
Rennel24 <24-00424@g.batstate-u.edu.ph>
2026-07-17 12:31:07 +0800

    added new files

---
+++ LESSONS/Functions/ArrowPractice.js
@@ -0,0 +1,28 @@
+
+
+const sayHello = () => { 
+    return "Hello World"; 
+}
+
+  console.log(sayHello()); 
+
+
+
+const add = (num1, num2) => {  
+    return `The sum of this number is:  ${num1 * num2}`;
+}
+
+console.log(add(5,6)); 
+
+
+const isEven = (num) => {
+     if(num % 2 === 0 ){ 
+        return `The num ${num} is even`; 
+     }
+     return `The nume ${num} is Odd`; 
+}
+
+    console.log(isEven(9)); 
+
+
+
