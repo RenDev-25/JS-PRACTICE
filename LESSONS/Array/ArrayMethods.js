3aa19fcf33a6d4e6193c41ca1922aefa1e6afca5
Rennel24 <24-00424@g.batstate-u.edu.ph>
2026-08-06 20:11:47 +0800

    added new files for js practice

---
+++ LESSONS/Array/ArrayMethods.js
@@ -0,0 +1,22 @@
+
+
+const fruits = ["Apple", "Banana"];
+fruits.push("Orange");     // push method 
+console.log(fruits);
+
+
+const grades = [85, 90, 95];   // remove the begginings
+grades.pop();
+console.log(grades);
+
+
+const queue = ["Customer B", "Customer C"];
+queue.unshift("Customer A");      // unshift() - Add to the Beginning
+console.log(queue);
+
+
+const tasks = ["Wake Up", "Eat Breakfast", "Study"]; // shift() - Remove the First Item
+tasks.shift();
+console.log(tasks);
+
+
